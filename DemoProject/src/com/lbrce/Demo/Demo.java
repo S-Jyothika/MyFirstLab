5bc8f157cbae0a7c53930a4c548aa410f1117cd5
poornima <tatikondapoornima18@gmail.com>
2024-08-22 10:43:59 +0530

    Update Demo.java

--- DemoProject/src/com/lbrce/Demo/Demo.java
+++ DemoProject/src/com/lbrce/Demo/Demo.java
@@ -7,3 +7,10 @@ public class Demo
 		System.out.println("Hello World");
 	}
 }
+public class Demo1
+	{
+		public static void main(String args[])
+		{
+			System.out.println("Hii this is first lab");
+		}
+	}
